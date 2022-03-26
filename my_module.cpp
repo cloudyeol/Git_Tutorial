96d586bd2ba171bfbfc14a0c430cf4f7704f77f3
cloudyeol <cloudyeol@naver.com>
2022-03-26 18:42:19 +0900

    add [add]

--- my_module.cpp
+++ my_module.cpp
@@ -6,15 +6,6 @@ int add(int a, int b){
 return a+b;
 };
 
-double sub(int a, int b){
-return a/b;
-};
-
-int mul (int a, int b){
-return a*b;
-};
-//main 주석 추가
-
 int main(){
 
 cout << "hello world" <<endl;
